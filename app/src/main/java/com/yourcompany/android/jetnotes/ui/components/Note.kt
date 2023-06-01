863bdd27b2e74e2119dedb2dd1bd664d9ee13c72
learnWithZak <learn.with.zak@gmail.com>
2023-06-01 05:02:43 +0200

    refactor: add new Note composable

---
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/components/Note.kt
@@ -0,0 +1,13 @@
+package com.yourcompany.android.jetnotes.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Note() {
+
+}
+
+@Composable
+private fun NotePreview() {
+    Note()
+}
