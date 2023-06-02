f6067e15fca12f9408778bc5f69c9daf90b708c7
learnWithZak <learn.with.zak@gmail.com>
2023-06-02 08:24:49 +0200

    refactor: add a Switch on/off of dark mode

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/components/AppDrawer.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -22,6 +23,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yourcompany.android.jetnotes.theme.JetNotesTheme
+import com.yourcompany.android.jetnotes.theme.JetNotesThemeSettings
 
 class AppDrawer {
 
@@ -97,6 +99,36 @@ class AppDrawer {
             ScreenNavigationButton(icon = Icons.Filled.Home, label = "Notes", isSelected = true) {
 
             }
+        }
+    }
+
+    @Composable
+    private fun LightDarkThemeItem() {
+        Row(Modifier.padding(8.dp)) {
+            Text(
+                text = "Turn on dark theme",
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            Switch(
+                checked = JetNotesThemeSettings.isDarkThemeEnabled,
+                onCheckedChange = { JetNotesThemeSettings.isDarkThemeEnabled = it },
+                modifier = Modifier
+                    .padding(start = 8.dp, end = 8.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun LightDarkThemeItemPreview() {
+        JetNotesTheme {
+            LightDarkThemeItem()
         }
     }
 }
