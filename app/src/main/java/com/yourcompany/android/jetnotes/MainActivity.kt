deb15c86aa300d4596c05e69dcff02a068de8493
learnWithZak <learn.with.zak@gmail.com>
2023-06-20 04:57:39 +0200

    refactor: use the AppDrawer inside a Scaffold

--- app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/MainActivity.kt
@@ -38,30 +38,52 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.yourcompany.android.jetnotes.routing.Screen
+import com.yourcompany.android.jetnotes.theme.JetNotesTheme
+import com.yourcompany.android.jetnotes.ui.components.AppDrawer
+import com.yourcompany.android.jetnotes.ui.components.Note
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
 
 /**
  * Main activity for the app.
  */
 class MainActivity : AppCompatActivity() {
 
-  private val viewModel: MainViewModel by viewModels(factoryProducer = {
-    MainViewModelFactory(
-      this,
-      (application as JetNotesApplication).dependencyInjector.repository
-    )
-  })
+    private val viewModel: MainViewModel by viewModels(factoryProducer = {
+        MainViewModelFactory(
+            this,
+            (application as JetNotesApplication).dependencyInjector.repository
+        )
+    })
 
-  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-  override fun onCreate(savedInstanceState: Bundle?) {
-    // Switch to AppTheme for displaying the activity
-    setTheme(R.style.Theme_JetNotes)
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Switch to AppTheme for displaying the activity
+        setTheme(R.style.Theme_JetNotes)
 
-    super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-    setContent {
+        setContent {
+            JetNotesTheme {
+                val coroutineScope = rememberCoroutineScope()
+                val scaffoldState = rememberScaffoldState()
 
+                Scaffold(scaffoldState = scaffoldState, drawerContent = {
+                    AppDrawer(currentScreen = Screen.Notes, onScreenSelected = { screen: Screen ->
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                    }
+                    )
+                }, content = {
+                    Note()
+                })
+            }
+        }
     }
-  }
 }
